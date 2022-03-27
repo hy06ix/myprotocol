5d2a9a5f4168bfb8958b53584477ff189c90268c
hy06ix <lhslhs9394@gmail.com>
2022-03-27 18:38:31 +0900

    edit import

--- simulation/simul/simul.go
+++ simulation/simul/simul.go
@@ -4,7 +4,7 @@ import (
 	// Service needs to be imported here to be instantiated.
 
 	"github.com/csanti/onet/simul"
-	_ "github.com/hy06ix/concordia"
+	_ "github.com/hy06ix/myprotocol"
 )
 
 func main() {
